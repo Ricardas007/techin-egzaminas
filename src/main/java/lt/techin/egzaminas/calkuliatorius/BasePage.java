0cf042a178ba278af5f836c71c4eaf7b31d154cc
Ricardas007 <rychmaister@gmail.com>
2024-06-26 09:32:24 +0300

    BasePage ,BaseTest clases, configuration

---
+++ src/main/java/lt/techin/egzaminas/calkuliatorius/BasePage.java
@@ -0,0 +1,19 @@
+package lt.techin.egzaminas.calkuliatorius;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+    public abstract class BasePage {
+
+        protected WebDriver driver;
+        protected WebDriverWait wait;
+
+        public BasePage(WebDriver driver) {
+            this.driver = driver;
+            PageFactory.initElements(driver, this);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        }
+}
---
+++ src/test/java/lt/techin/egzaminas/calkuliatorius/test/BaseTest.java
@@ -0,0 +1,24 @@
+package lt.techin.egzaminas.calkuliatorius.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://testpages.eviltester.com/styled/calculator");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterEach
+    public void tearsDown() {
+        driver.quit();
+    }
+}
